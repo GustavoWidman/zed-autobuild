ed00babecc7724599eb314b3ecb927063c549308
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-09 00:08:37 -0300

    feat(flake): add caches and nixpkgs overlay

--- flake.nix
+++ flake.nix
@@ -5,5 +5,25 @@
     zed.url = "github:zed-industries/zed/v0.208.1-pre";
   };
 
-  outputs = { zed, ... }: zed.outputs;
+  outputs =
+    { zed, ... }:
+    zed.outputs
+    // {
+      overlays.default = final: prev: {
+        zed-editor = zed.outputs.packages.${final.system}.default;
+      };
+    };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://r3dlust.cachix.org"
+      "https://cache.garnix.io"
+      "https://zed.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "r3dlust.cachix.org-1:/R3S8pW/nr7kOBJKcGPsZ0zCepvldTUEgbrqa4O3cW0="
+      "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 }
